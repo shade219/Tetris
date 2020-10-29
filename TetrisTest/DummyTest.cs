fc280eaee90c5d81bdccd763718a543785572566
gkulasik-depaul <71193278+gkulasik-depaul@users.noreply.github.com>
2020-10-28 22:30:02 -0500

    Fix directory structure + initial failing test

---
+++ TetrisTest/DummyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TetrisTest
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void SanityTest()
+        {
+            // intentionally failing test
+            Assert.IsTrue(false)
+        }
+    }
+}
