458db1c28828fb4de405c69a6d08b644bc698c61
gkulasik-depaul <71193278+gkulasik-depaul@users.noreply.github.com>
2020-10-28 22:48:13 -0500

    Start adding in stub classes

---
+++ Tetris/domain/Block.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.domain
+{
+    public class Block
+    {
+        DrawColor.Shade color;
+        Vector2 coordinates;
+
+        public Block copy()
+        {
+            return null;
+        }
+    }
+}
