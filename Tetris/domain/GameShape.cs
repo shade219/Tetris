492c5e6b774760913aa1b16859bea9756f55a033
gkulasik-depaul <71193278+gkulasik-depaul@users.noreply.github.com>
2020-10-28 23:23:34 -0500

    Create GameShape.cs

---
+++ Tetris/domain/GameShape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.domain
+{
+    public abstract class GameShape
+    {
+        Block anchor;
+        List<Block> blocks;
+        Shape.Orientation orientation;
+        DrawColor.Shade color;
+        bool isAboutToPlace;
+        bool isPlaced;
+
+        public abstract void draw();
+
+        public abstract void applyAction();
+
+        public abstract List<Block> calcBlocksPostAction();
+
+
+    }
+}
