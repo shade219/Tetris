16c527f0abdf6168a7e02125be1976e0ca707097
anaanghel <71126080+anaanghel@users.noreply.github.com>
2020-11-02 15:19:34 +0200

    updated start and end values

--- Tetris/services/Timer.cs
+++ Tetris/services/Timer.cs
@@ -20,6 +20,8 @@ namespace Tetris.services
         public Timer(int duration)
         {
             this.duration = duration;
+            this.start = -1;
+            this.end = -1;
         }
 
         // Author: Ana Maria Anghel
