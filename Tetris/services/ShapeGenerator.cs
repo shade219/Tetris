4ec8a363273ab63ccb9db59fc742e947960ef990
yliu202 <yliu202@depaul.edu>
2020-11-02 02:25:38 -0600

    ShapeGenerator code and unit tests

--- Tetris/services/ShapeGenerator.cs
+++ Tetris/services/ShapeGenerator.cs
@@ -1,21 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Tetris.domain;
+using Tetris.domain.shapes;
 
 namespace Tetris.services
 {
-    // Authors: Name1, Name2
-    // Description:
+    // Authors: Yao-Hua Liu
+    // Description: Static Class to Generate next GameShape. 
+    //              Probability is calculated based on current level.
+    //              The anchor coordinate is set to be in the preview window.
     public static class ShapeGenerator
     {
+        // Author: Yao-Hua Liu
+        private static readonly int ANCHOR_X = Constants.PREVIEW_WINDOW_X;
+        private static readonly int ANCHOR_Y = Constants.PREVIEW_WINDOW_X;
 
-        // Author: Your Name Here
         public static GameShape GenerateShape(int currentLevel)
         {
-            return null;
+            Random rnd = new Random();
+            int result = rnd.Next(1, 1000);
+
+            return PrivateGenerateShape(currentLevel, result);
+        }
+
+        private static GameShape PrivateGenerateShape(int currentLevel, int result)
+        {
+            int SquareAndL12Bound = SquareAndL12ShapeProbability(currentLevel);
+            int LineShapeBound = LineShapeProbability();
+            int TAndZ12Bound = TAndZ12ShapeProbability(currentLevel);
+
+            Vector2 pos = new Vector2(ANCHOR_X, ANCHOR_Y);
+
+            if (result < SquareAndL12Bound)
+            {
+                return new SquareShape(new Block(DrawColor.getColor(DrawColor.Shade.COLOR_DK_RED), pos));
+            }
+            else if (result < SquareAndL12Bound * 2)
+            {
+                return new L1Shape(new Block(DrawColor.getColor(DrawColor.Shade.COLOR_DK_BLUE), pos));
+            }
+            else if (result < SquareAndL12Bound * 3)
+            {
+                return new L2Shape(new Block(DrawColor.getColor(DrawColor.Shade.COLOR_DK_PURPLE), pos));
+            }
+            else if (result < SquareAndL12Bound * 3 + LineShapeBound)
+            {
+                return new LineShape(new Block(DrawColor.getColor(DrawColor.Shade.COLOR_DK_ORANGE), pos));
+            }
+            else if (result < SquareAndL12Bound * 3 + LineShapeBound + TAndZ12Bound)
+            {
+                return new TShape(new Block(DrawColor.getColor(DrawColor.Shade.COLOR_DK_YELLOW), pos));
+            }
+            else if (result < SquareAndL12Bound * 3 + LineShapeBound + TAndZ12Bound * 2)
+            {
+                return new Z1Shape(new Block(DrawColor.getColor(DrawColor.Shade.COLOR_DK_GREEN), pos));
+            }
+            else
+            {
+                return new Z2Shape(new Block(DrawColor.getColor(DrawColor.Shade.COLOR_DK_CYAN), pos));
+            }
+        }
+
+        private static int SquareAndL12ShapeProbability(int currentLevel)
+        {
+            return (int)((1.0f / 7.0f - ((float)(currentLevel - 4) / 75.0f)) * 1000.0f);
+        }
+
+        private static int LineShapeProbability()
+        {
+            return (int)((1.0f / 7.0f) * 1000.0f);
+        }
+
+        private static int TAndZ12ShapeProbability(int currentLevel)
+        {
+            return (int)((1.0f / 7.0f + ((float)(currentLevel - 4) / 75.0f)) * 1000.0f);
         }
     }
 }
--- TetrisTest/services/ShapeGeneratorTests.cs
+++ TetrisTest/services/ShapeGeneratorTests.cs
@@ -5,19 +5,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tetris.domain;
+using Tetris.domain.shapes;
 
 namespace Tetris.services.Tests
 {
-    // Authors: Name1, Name2
-    // Description:
+    // Authors: Yao-Hua Liu
+    // Description: Unit Test for ShapeGenerator methods
     [TestClass()]
     public class ShapeGeneratorTests
     {
-        // Author: Your Name Here
+        // Author: Yao-Hua Liu
         [TestMethod()]
         public void GenerateShapeTest()
         {
-
+            GameShape textShape = ShapeGenerator.GenerateShape(1);
+            Assert.IsTrue(textShape is GameShape);
         }
+
+        [TestMethod()]
+        [DataRow(1, 100, typeof(SquareShape))]
+        [DataRow(1, 200, typeof(L1Shape))]
+        [DataRow(1, 400, typeof(L2Shape))]
+        [DataRow(1, 600, typeof(LineShape))]
+        [DataRow(1, 700, typeof(TShape))]
+        [DataRow(1, 800, typeof(Z1Shape))]
+        [DataRow(1, 900, typeof(Z2Shape))]
+        [DataRow(10, 100, typeof(L1Shape))]
+        [DataRow(10, 200, typeof(LineShape))]
+        [DataRow(10, 400, typeof(TShape))]
+        [DataRow(10, 600, typeof(Z1Shape))]
+        [DataRow(10, 700, typeof(Z1Shape))]
+        [DataRow(10, 800, typeof(Z2Shape))]
+        [DataRow(10, 900, typeof(Z2Shape))]
+        public void PrivateGenerateShapeTest(int inLevel, int randResult, Type type)
+        {
+            PrivateType wrapper = new PrivateType(typeof(ShapeGenerator));
+            GameShape result = (GameShape)wrapper.InvokeStatic("PrivateGenerateShape", inLevel, randResult);
+            Assert.IsTrue(result.GetType() == type);
+        }
+
+        [TestMethod()]
+        [DataRow(1, 182)]
+        [DataRow(2, 169)]
+        [DataRow(3, 156)]
+        [DataRow(4, 142)]
+        [DataRow(5, 129)]
+        [DataRow(6, 116)]
+        [DataRow(7, 102)]
+        [DataRow(8, 89)]
+        [DataRow(9, 76)]
+        [DataRow(10, 62)]
+        public void SquareAndL12ShapeProbabilityTest(int inLevel, float expectedProbability)
+        {
+            PrivateType wrapper = new PrivateType(typeof(ShapeGenerator));
+            int result = (int)wrapper.InvokeStatic("SquareAndL12ShapeProbability", inLevel);
+            Assert.AreEqual(expectedProbability, result);
+        }
+
+        [TestMethod()]
+        public void LineShapeProbabilityTest()
+        {
+            PrivateType wrapper = new PrivateType(typeof(ShapeGenerator));
+            int result = (int)wrapper.InvokeStatic("LineShapeProbability");
+            Assert.AreEqual(142, result);
+        }
+
+        [TestMethod()]
+        [DataRow(1, 102)]
+        [DataRow(2, 116)]
+        [DataRow(3, 129)]
+        [DataRow(4, 142)]
+        [DataRow(5, 156)]
+        [DataRow(6, 169)]
+        [DataRow(7, 182)]
+        [DataRow(8, 196)]
+        [DataRow(9, 209)]
+        [DataRow(10, 222)]
+        public void TAndZ12ShapeProbabilityTest(int inLevel, float expectedProbability)
+        {
+            PrivateType wrapper = new PrivateType(typeof(ShapeGenerator));
+            int result = (int)wrapper.InvokeStatic("TAndZ12ShapeProbability", inLevel);
+            Assert.AreEqual(expectedProbability, result);
+        }
+
     }
 }
