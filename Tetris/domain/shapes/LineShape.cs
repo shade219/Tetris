0b704a1ddfc41459efdef6081dac7a9385952ac5
Alex <ASCHERTL@depaul.edu>
2020-11-03 17:19:48 -0600

    shape rotations

--- Tetris/domain/shapes/LineShape.cs
+++ Tetris/domain/shapes/LineShape.cs
@@ -1,26 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tetris.domain.shapes
 {
-    // Authors: Name1, Name2
-    // Description:
+    // Authors: Alex Schertler
+    // Description: Rotates the Line shape around the anchor point
     public class LineShape : GameShape
     {
 
-        // Author: Your Name Here
+        // Author: Alex Schertler
         public LineShape(Block anchor, ShapeRenderer.Orientation orientation) : base(anchor, orientation)
         {
+            //this.anchor = anchor;
 
+            // Check ShapeRenderer.draw___ which will tell you the color of the shape
+            this.color = ShapeRenderer.GetLineColor();
+
+            // Draw a grid and determine what the shape looks like when it is at 0, 90, 180, and 270 degrees.
+            // Then determine the other block offsets. The anchor is (x, y), the block immediately on the right is (x+1, y), the block immediately below is (x, y-1), etc.
+            // Handle adding/creating the other blocks for each potential orientation
+            this.blocks.Add(anchor);
+            //NOTE:: Swapped ordering of anchors <==> Orientations ---> matching ShapeRenderer*
+            switch (orientation)
+            {
+                case ShapeRenderer.Orientation.ORIENT_0: // 0 - anchor is second to left
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, 0))); // 1 (block to left)
+                    this.blocks.Add(anchor.Copy(new Vector2(1, 0))); // 3 (block to close right)
+                    this.blocks.Add(anchor.Copy(new Vector2(2, 0))); // 4 (block to far right)
+                    break;
+                case ShapeRenderer.Orientation.ORIENT_1: // 90 - anchor is second from top
+                    this.blocks.Add(anchor.Copy(new Vector2(0, 1))); // 1 (block above)
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -1))); // 3 (block to close below)
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -2))); // 4 (block to far below)
+                    break;
+                case ShapeRenderer.Orientation.ORIENT_2: // 180 - anchor is second to left
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, 0))); // 1 (block to left)
+                    this.blocks.Add(anchor.Copy(new Vector2(1, 0))); // 3 (block to close right)
+                    this.blocks.Add(anchor.Copy(new Vector2(2, 0))); // 4 (block to far right)
+                    break;
+                case ShapeRenderer.Orientation.ORIENT_3: // 270 - anchor is second from top
+                    this.blocks.Add(anchor.Copy(new Vector2(0, 1))); // 1 (block above)
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -1))); // 3 (block to close below)
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -2))); // 4 (block to far below)
+                    break;
+                default:
+                    throw new ArgumentException("Unexpected ShapeRenderer::Orientation in SquareShape constructor: " + orientation);
+            }
+
+            //rotation offset dictionary
+            this.nextOriToOffsets = new Dictionary<ShapeRenderer.Orientation, List<Vector2>>();
+
+            // Note: Set the first Vector2 to (0, 0) to not move the anchor
+            // 270 -> 0
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_0, new[] { new Vector2(0, 0), new Vector2(-1, -1), new Vector2(1, 1), new Vector2(2, 2) }.ToList());
+            // 0 -> 90
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_1, new[] { new Vector2(0, 0), new Vector2(1, 1), new Vector2(-1, -1), new Vector2(-2, -2) }.ToList());
+            // 90 -> 180 (same as 270 -> 0)
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_2, new[] { new Vector2(0, 0), new Vector2(-1, -1), new Vector2(1, 1), new Vector2(2, 2) }.ToList());
+            // 180 -> 270 (same as 0 -> 90)
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_3, new[] { new Vector2(0, 0), new Vector2(1, 1), new Vector2(-1, -1), new Vector2(-2, -2) }.ToList());
         }
 
-        // Author: Your Name Here
+
+        // Author: Alex Schertler
         public override void Draw()
         {
-            throw new NotImplementedException();
+            ShapeRenderer.drawLine(anchor.GetX(), anchor.GetY(), orientation);
         }
     }
 }
--- Tetris/domain/shapes/Z2Shape.cs
+++ Tetris/domain/shapes/Z2Shape.cs
@@ -1,26 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tetris.domain.shapes
 {
-    // Authors: Name1, Name2
-    // Description:
+    // Authors: Alex Schertler
+    // Description: Rotates the Z2 shape around its anchor
     public class Z2Shape : GameShape
     {
 
-        // Author: Your Name Here
+        // Author: Alex Schertler
         public Z2Shape(Block anchor, ShapeRenderer.Orientation orientation) : base(anchor, orientation)
         {
+            //this.anchor = anchor;
 
+            // Check ShapeRenderer.draw___ which will tell you the color of the shape
+            this.color = ShapeRenderer.GetZ2Color();
+
+            // Draw a grid and determine what the shape looks like when it is at 0, 90, 180, and 270 degrees.
+            // Then determine the other block offsets. The anchor is (x, y), the block immediately on the right is (x+1, y), the block immediately below is (x, y-1), etc.
+            // Handle adding/creating the other blocks for each potential orientation
+            this.blocks.Add(anchor);
+            //NOTE:: Swapped ordering of anchors <==> Orientations ---> matching ShapeRenderer*
+            switch (orientation)
+            {
+                case ShapeRenderer.Orientation.ORIENT_0: // 0 - anchor is on the top of middle collumn
+                    this.blocks.Add(anchor.Copy(new Vector2(1, 0))); // 3 (block to right)
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -1))); // 4 (block below)
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, -1))); // 1 (block below and to left)
+                    break;
+                case ShapeRenderer.Orientation.ORIENT_1: // 90 - anchor is on the top of right collumn
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -1))); // 3 (block below)
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, 0))); // 4 (block to left)
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, 1))); // 1 (block above and to left)
+                    break;
+                case ShapeRenderer.Orientation.ORIENT_2: // 180 - anchor is on the top of middle collumn
+                    this.blocks.Add(anchor.Copy(new Vector2(1, 0))); // 3 (block to right)
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -1))); // 4 (block below)
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, -1))); // 1 (block below and to left)
+                    break;
+                case ShapeRenderer.Orientation.ORIENT_3: // 270 - anchor is on the top of right collumn
+                    this.blocks.Add(anchor.Copy(new Vector2(0, -1))); // 3 (block below)
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, 0))); // 4 (block to left)
+                    this.blocks.Add(anchor.Copy(new Vector2(-1, 1))); // 1 (block above and to left)
+                    break;
+                default:
+                    throw new ArgumentException("Unexpected ShapeRenderer::Orientation in SquareShape constructor: " + orientation);
+            }
+
+            //rotation offset dictionary
+            this.nextOriToOffsets = new Dictionary<ShapeRenderer.Orientation, List<Vector2>>();
+
+            // Note: Set the first Vector2 to (0, 0) to not move the anchor
+            // 270 -> 0
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_0, new[] { new Vector2(0, 0), new Vector2(1, 1), new Vector2(1, -1), new Vector2(0, -2) }.ToList());
+            // 0 -> 90
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_1, new[] { new Vector2(0, 0), new Vector2(-1, -1), new Vector2(-1, 1), new Vector2(0, 2) }.ToList());
+            // 90 -> 180 (same as 270 -> 0)
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_2, new[] { new Vector2(0, 0), new Vector2(1, 1), new Vector2(1, -1), new Vector2(0, -2) }.ToList());
+            // 180 -> 270 (same as 0 -> 90)
+            nextOriToOffsets.Add(ShapeRenderer.Orientation.ORIENT_3, new[] { new Vector2(0, 0), new Vector2(-1, -1), new Vector2(-1, 1), new Vector2(0, 2) }.ToList());
         }
 
-        // Author: Your Name Here
+
+        // Author: Alex Schertler
         public override void Draw()
         {
-            throw new NotImplementedException();
+        ShapeRenderer.drawZ2(anchor.GetX(), anchor.GetY(), orientation);
         }
     }
 }
