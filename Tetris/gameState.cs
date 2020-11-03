c85cee5a6eff3a659b8877a77f2ab32af2fc5093
mvolland224 <mvolland@depaul.edu>
2020-11-03 11:08:54 -0600

    gameState first code efforts

---
+++ Tetris/gameState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris.domain;
+
+namespace Tetris
+{
+    class gameState
+    { 
+        GameShape activeShape;
+        BlockGrid grid;
+        GameShape nextShape;
+        public int totalLinesCleared;
+        public int currentLevel;
+        public int currentScore;
+
+        public gameState()
+        {
+            activeShape = new activeShape(); 
+            this.grid = new BlockGrid(); 
+            nextShape = new nextShape();
+            totalLinesCleared = new totalLinesCleared();
+            currentLevel = new currentLevel();
+            currentScore = new currentScore();
+        }
+    }
+
+}
