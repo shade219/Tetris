4a81b17235da4cfd66f287dc3a95c05372717b29
sstahl-depaul <sstahl2@depaul.edu>
2020-11-03 14:03:19 -0600

    fixed git conflic syntax relic

--- Tetris/domain/shapes/GameShape.cs
+++ Tetris/domain/shapes/GameShape.cs
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris.domain
-{
-    // Authors: Name1, Name2
-    // Description:
-    public abstract class GameShape
-    {
-        Block anchor;
-        List<Block> blocks;
-        ShapeRenderer.Orientation orientation;
-        DrawColor.Shade color;
-        public bool isAboutToPlace;
-        public bool isPlaced;
-
-        public GameShape(Block anchor)
-        {
-            this.anchor = anchor;
-        }
-
-        public abstract void Draw();
-
-        public abstract void ApplyAction(InputAction action);
-
-        public abstract List<Block> CalcBlocksPostAction(InputAction action);
-
-
-    }
-}
-=======
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -268,4 +233,3 @@ namespace Tetris.domain
 
     }
 }
->>>>>>> origin/sam-movementmanager
