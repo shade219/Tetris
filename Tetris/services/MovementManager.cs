fb8cb75f8536ee2c96674d03bc92faf8812c84f6
sstahl-depaul <sstahl2@depaul.edu>
2020-11-03 12:46:56 -0600

    refactored MovementManager logic based on team-5 implementation and fixed logic to handle collisions correctly

--- Tetris/services/MovementManager.cs
+++ Tetris/services/MovementManager.cs
@@ -8,48 +8,72 @@ using Tetris.domain;
 
 namespace Tetris.services
 {
-    // Authors: Name1, Name2
-    // Description:
+    // Authors: Samuel Stahl, Brandon Wegner
+    // Description: processes result of InputAction on GameShape in relation to BlockGrid and performs collision checking
     public static class MovementManager
     {
 
-        // Author: Your Name Here
+        // Author: Brandon Wegner
+        //Checks for collision in two states:
+
+        //State 1: about to place - If AboutToPlace flag is set and action is MoveDown call placeshape
+        //else calculates action as usual to check for legal action and apply accordingly
+
+        //State 2: not about to place - Check's for collision and returns true if action not legal
+        // else returns false and calls GameShape.ApplyAction()
         public static void ApplyAction(InputAction action, BlockGrid grid, GameShape shape)
         {
-            if (shape.isAboutToPlace && CheckForCollisions(grid, shape.CalcBlocksPostAction(action), shape, action))
+            if (shape.isAboutToPlace)
             {
-                grid.PlaceShape(shape);
+                if(action == InputAction.MoveDown)
+                {
+                    grid.PlaceShape(shape);
+                }
+                else
+                {
+                    List<Block> shapeBlocks = shape.CalcBlocksPostAction(action);
+                    bool collision = CheckForCollisions(grid, shapeBlocks, shape, action);
+                    if (!collision)
+                    {
+                        shape.ApplyAction(action);
+                    }
+                }
             }
             else
             {
                 List<Block> shapeBlocks = shape.CalcBlocksPostAction(action);
                 bool collision = CheckForCollisions(grid, shapeBlocks, shape, action);
-                if (collision)
-                {
-                    return;
-                }
-                else
+                if (!collision)
                 {
                     shape.ApplyAction(action);
                 }
             }
         }
 
-        // Author: Your Name Here
+        // Author: Samuel Stahl
+        //Checks first for illegal move and returns true if found
+        // else returns false if move is legal
+        //After illegal move check, checks for imenent collision with bottom or block in grid and sets AboutToPlace if true
         public static bool CheckForCollisions(BlockGrid grid, List<Block> blocks, GameShape shape, InputAction action)
         {
             List<Vector2> occupiedCoordinates = grid.GetOccupiedCoordinates();
 
+            //Checks for illegal collisions with boundary or occupied areas and returns true if collision found
             foreach (Block b in blocks)
             {
-                Vector2 blockCoord = b.coordinates;
+                Vector2 blockCoord = b.GetPosition();
                 //left boundary check
                 if (blockCoord.X < 0)
                 {
                     return true;
                 }
                 //right boundary check
-                else if (blockCoord.X > grid.rowLength())
+                else if (blockCoord.X >= grid.GetGridColumnCount())
+                {
+                    return true;
+                }
+                //bottom boundary check
+                else if (blockCoord.Y < 0)
                 {
                     return true;
                 }
@@ -58,7 +82,20 @@ namespace Tetris.services
                 {
                     return true;
                 }
-                //above occupied coord check
+
+            }
+
+            //Checks for legal collisions that will cause block placement next timer tick
+            //If legal collision is imenent applies the action and sets about to place flag
+            foreach(Block b in blocks)
+            {
+                Vector2 blockCoord = b.GetPosition();
+                //check for bottom row collision
+                if (blockCoord.Y == 0)
+                {
+                    shape.AboutToPlaceGameShape();
+                }
+                //above occupied coord collision check
                 else
                 {
                     Vector2 temp;
@@ -67,9 +104,7 @@ namespace Tetris.services
                     temp.Y = blockCoord.Y - 1;
                     if (occupiedCoordinates.Contains(temp))
                     {
-                        shape.ApplyAction(action);
-                        shape.isAboutToPlace = true;
-                        return true;
+                        shape.AboutToPlaceGameShape();
                     }
                     return false;
                 }
