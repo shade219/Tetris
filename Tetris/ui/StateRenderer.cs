2cc75435a7805f4b439b120a2afc33c66c762858
Tomasz Zdanowski <tom.zdan9850@gmail.com>
2020-11-04 06:42:11 -0600

    added pseudocode, project won't compile due to unrelated error

--- Tetris/ui/StateRenderer.cs
+++ Tetris/ui/StateRenderer.cs
@@ -8,22 +8,44 @@ using Tetris.domain;
 namespace Tetris.ui
 {
     // Authors: Nathan Hester, Tom Zdanowski
- /* Description:
-    Uses GameState and UI classes like ShapeRenderer, 
-    Constants, and DrawColor to convert the logical GameState into a
-    visual represenatition of Tetris.
 
-    EX: StateRenderer draw() should fill in the grid, update the level, score, and etc..
+    /* Description:
+        Uses GameState, GameManager, and UI classes like ShapeRenderer, 
+        Constants, and DrawColor (maybe SpriteObjectManager) to convert the 
+        logical GameState into a visual represenatition of Tetris.
+        EX: StateRenderer draw() should fill in the grid, update the level, score, and etc..
     */
     public static class StateRenderer
     {
 
-        // Author: Your Name Here
+        // Author: Tom Zdanowski
         public static void Draw(GameState state)
         {
             //DrawColor drawColor = new DrawColor();
+
+            // 1. Sets up the so called background which involves the container and preview window
+  
             SOM.drawBackground();
-        
+
+            // 2. add activeShape and nextShape either via external method or...
+            
+            // GameShape activeShape =
+            // GameShape nextShape = 
+
+            // 3. fill in grid for tetris board
+            // Should call a BlockGrid method or gameState info for this
+
+            // BlockGrid bg = 
+
+            // 4. add strings to showcase the currentLevel, currentScore, and totalLinesCleared
+
+            // state.currentLevel
+            // state.currentScore
+            // state.TotalLinesCleared
+
+            // 5. To Be Continued
+
+
         }
     }
 }
